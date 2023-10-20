8dab30c48e1a872134241f379abc4b347d64639f
Ruizhe-Yang <yang.ruizhe@outlook.com>
2023-10-20 16:49:04 +0800

    .

--- cnsa.ee.digital.twin.design/src/cnsa/ee/digital/twin/design/com/net/TCPClient.java
+++ cnsa.ee.digital.twin.design/src/cnsa/ee/digital/twin/design/com/net/TCPClient.java
@@ -98,63 +98,63 @@ public class TCPClient extends Frame{
 		}
 	}
 	
-	public class TCPServer {
-		static DataInputStream dis = null;
-		public static void main(String[] args){
-			System.out.println("OK");
-			boolean started = false;
-			Socket s = null;
-			TextArea ta = new TextArea();
-			ta.append("从客户端接受的数据:"+"\n");
-			ServerSocket ss = null;
-			try{
-				ss = new ServerSocket(8889);//端口号
-			} catch (BindException e){
-				System.exit(0);
-			} catch (IOException e){
-				e.printStackTrace();
-			}
-			Frame f = new Frame("服务器端");//窗体名称	
-			f.setLocation(400, 300);//窗体出现位置
-			f.setSize(300, 300);//窗体大小
-			f.add(ta,BorderLayout.NORTH);
-			f.pack();
-			f.addWindowListener(
-				new WindowAdapter(){
-					public void windowClosing(WindowEvent e){
-						System.exit(0);
-						}
-					}
-				);
-			f.setVisible(true);
-			try{			 //try-catch块捕捉异常
-				started = true;
-				while(started){
-					boolean bConnected = false;
-					s = ss.accept();
-					bConnected = true;
-					dis = new DataInputStream(s.getInputStream());
-					while(bConnected){
-					    String str = dis.readUTF();
-					    ta.append(str+"\n");
-					}
-				}
-			}catch(EOFException e){                                 
-				System.out.println("Client closed!");
-			}catch(IOException e){
-				e.printStackTrace();
-			}finally{
-				try{
-					if(dis != null)
-						dis.close();
-					if(s != null)
-						s.close();
-				}catch(Exception e){
-					e.printStackTrace();
-				}
-			}
-		}
-	}
+//	public class TCPServer {
+//		static DataInputStream dis = null;
+//		public static void main(String[] args){
+//			System.out.println("OK");
+//			boolean started = false;
+//			Socket s = null;
+//			TextArea ta = new TextArea();
+//			ta.append("从客户端接受的数据:"+"\n");
+//			ServerSocket ss = null;
+//			try{
+//				ss = new ServerSocket(8889);//端口号
+//			} catch (BindException e){
+//				System.exit(0);
+//			} catch (IOException e){
+//				e.printStackTrace();
+//			}
+//			Frame f = new Frame("服务器端");//窗体名称	
+//			f.setLocation(400, 300);//窗体出现位置
+//			f.setSize(300, 300);//窗体大小
+//			f.add(ta,BorderLayout.NORTH);
+//			f.pack();
+//			f.addWindowListener(
+//				new WindowAdapter(){
+//					public void windowClosing(WindowEvent e){
+//						System.exit(0);
+//						}
+//					}
+//				);
+//			f.setVisible(true);
+//			try{			 //try-catch块捕捉异常
+//				started = true;
+//				while(started){
+//					boolean bConnected = false;
+//					s = ss.accept();
+//					bConnected = true;
+//					dis = new DataInputStream(s.getInputStream());
+//					while(bConnected){
+//					    String str = dis.readUTF();
+//					    ta.append(str+"\n");
+//					}
+//				}
+//			}catch(EOFException e){                                 
+//				System.out.println("Client closed!");
+//			}catch(IOException e){
+//				e.printStackTrace();
+//			}finally{
+//				try{
+//					if(dis != null)
+//						dis.close();
+//					if(s != null)
+//						s.close();
+//				}catch(Exception e){
+//					e.printStackTrace();
+//				}
+//			}
+//		}
+//	}
 	
 }
 
